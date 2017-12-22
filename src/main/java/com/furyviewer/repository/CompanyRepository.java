c5cbfc44b20e82b0eb15664a90e064ecde026d0c
Whoger <gonzalo.toscano.h@gmail.com>
2017-12-22 09:57:47 +0100

    #1 Find Companies by name

--- src/main/java/com/furyviewer/repository/CompanyRepository.java
+++ src/main/java/com/furyviewer/repository/CompanyRepository.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
-
+import java.util.List;
 /**
  * Spring Data JPA repository for the Company entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    List<Company>findCompanyByName(String name);
 
 }
