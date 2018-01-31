e55460c6a9ea10268dc36006de582c27f1a8fe2f
IFriedkin <ibrahim.friedkin@gmail.com>
2018-01-31 13:52:10 +0100

    #13 Corregido el bug que permitía guardar artistas como N/A en caso de ser null.     -Añadidos comentarios a ArtistService.

--- src/main/java/com/furyviewer/service/ArtistService.java
+++ src/main/java/com/furyviewer/service/ArtistService.java
@@ -21,75 +21,110 @@ public class ArtistService {
     @Autowired
     private ArtistTypeRepository artistTypeRepository;
 
+    /**
+     * Método que se encarga de convertir un String en los objetos de la clase Artist necesarios que contiene su nombre
+     * y main_actor como tipo de artista.
+     * @param actorsListStr String | Contiene el nombre de los artistas.
+     * @return Set<Artist> | Set que contiene la información de los artistas.
+     */
     public Set<Artist> importActors(String actorsListStr){
-        String[] actors = actorsListStr.split(", ");
         Set<Artist> artists = new HashSet<>();
-        ArtistType atMainActor = artistTypeRepository.findByName(ArtistTypeEnum.MAIN_ACTOR);
-        for(String actorStr: actors){
-            Optional<Artist> optionalActor = artistRepository.findByName(actorStr);
-            Artist artist;
-            if(optionalActor.isPresent()){
-                artist = optionalActor.get();
-                if(!artist.getArtistTypes().contains(atMainActor)){
+
+        if (!actorsListStr.equalsIgnoreCase("N/A")) {
+            String[] actors = actorsListStr.split(", ");
+            ArtistType atMainActor = artistTypeRepository.findByName(ArtistTypeEnum.MAIN_ACTOR);
+
+            for (String actorStr : actors) {
+                Optional<Artist> optionalActor = artistRepository.findByName(actorStr);
+                Artist artist;
+
+                //En caso de que el artista exista se comprueba si ya tiene asignado el tipo main_actor.
+                if (optionalActor.isPresent()) {
+                    artist = optionalActor.get();
+
+                    if (!artist.getArtistTypes().contains(atMainActor)) {
+                        artist.addArtistType(atMainActor);
+                        artist = artistRepository.save(artist);
+                    }
+
+                } else {
+                    //Se crea un artista desde cero.
+                    artist = new Artist();
+                    artist.setName(actorStr);
                     artist.addArtistType(atMainActor);
                     artist = artistRepository.save(artist);
                 }
 
-            }else{
-                artist = new Artist();
-                artist.setName(actorStr);
-                artist.addArtistType(atMainActor);
-                artist = artistRepository.save(artist);
+                artists.add(artist);
             }
-
-            artists.add(artist);
         }
         return artists;
-
     }
 
+    /**
+     * Método que se encarga de convertir un String en un objeto de la clase Artist que contiene su nombre y
+     * director como tipo de artista.
+     * @param director String | Contiene el nombre del artista.
+     * @return Artist | Objeto que contiene la información del artista.
+     */
     public Artist importDirector(String director){
-        Optional<Artist> optionalDirector = artistRepository.findByName(director);
-        Artist artist;
-        ArtistType atDirector = artistTypeRepository.findByName(ArtistTypeEnum.DIRECTOR);
-        if(optionalDirector.isPresent()){
-            artist = optionalDirector.get();
+        Artist artist = null;
 
-            if(!artist.getArtistTypes().contains(atDirector)){
+        if (!director.equalsIgnoreCase("N/A")) {
+            Optional<Artist> optionalDirector = artistRepository.findByName(director);
+            ArtistType atDirector = artistTypeRepository.findByName(ArtistTypeEnum.DIRECTOR);
+
+            //En caso de que el artista exista se comprueba si ya tiene asignado el tipo main_actor.
+            if(optionalDirector.isPresent()){
+                artist = optionalDirector.get();
+
+                if(!artist.getArtistTypes().contains(atDirector)){
+                    artist.addArtistType(atDirector);
+                    artistRepository.save(artist);
+                }
+
+            }else{
+                //Se crea un artista desde cero.
+                artist = new Artist();
+                artist.setName(director);
                 artist.addArtistType(atDirector);
-                artistRepository.save(artist);
+                artist = artistRepository.save(artist);
             }
-
-        }else{
-            artist = new Artist();
-            artist.setName(director);
-            artist.addArtistType(atDirector);
-            artist = artistRepository.save(artist);
         }
 
         return artist;
     }
 
-    public Artist importScripwriter(String escitor){
-        String[] scripwriterArray = escitor.split(",| \\(");
+    /**
+     * Método que se encarga de convertir un String en un objeto de la clase Artist que contiene su nombre y
+     * scripwriter como tipo de artista.
+     * @param scripwriter String | Contiene el nombre del artista.
+     * @return Artist | Objeto que contiene la información del artista.
+     */
+    public Artist importScripwriter(String scripwriter){
+        Artist artist = null;
 
-        Optional<Artist> optionalScripwriter = artistRepository.findByName(scripwriterArray[0]);
-        Artist artist;
-        ArtistType atScripwriter = artistTypeRepository.findByName(ArtistTypeEnum.SCRIPTWRITER);
+        if (!scripwriter.equalsIgnoreCase("N/A")) {
+            String[] scripwriterArray = scripwriter.split(",| \\(");
+            Optional<Artist> optionalScripwriter = artistRepository.findByName(scripwriterArray[0]);
+            ArtistType atScripwriter = artistTypeRepository.findByName(ArtistTypeEnum.SCRIPTWRITER);
 
+            //En caso de que el artista exista se comprueba si ya tiene asignado el tipo main_actor.
+            if (optionalScripwriter.isPresent()) {
+                artist = optionalScripwriter.get();
 
-        if(optionalScripwriter.isPresent()){
-            artist = optionalScripwriter.get();
+                if (!artist.getArtistTypes().contains(atScripwriter)) {
+                    artist.addArtistType(atScripwriter);
+                    artistRepository.save(artist);
+                }
 
-            if(!artist.getArtistTypes().contains(atScripwriter)){
+            } else {
+                //Se crea un artista desde cero.
+                artist = new Artist();
+                artist.setName(scripwriterArray[0]);
                 artist.addArtistType(atScripwriter);
-                artistRepository.save(artist);
+                artist = artistRepository.save(artist);
             }
-        }else{
-            artist = new Artist();
-            artist.setName(scripwriterArray[0]);
-            artist.addArtistType(atScripwriter);
-            artist = artistRepository.save(artist);
         }
 
         return artist;
