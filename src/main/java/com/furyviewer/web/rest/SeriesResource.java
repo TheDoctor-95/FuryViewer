100a981eef1d4bb4d048e262475630e28fbc98ad
IFriedkin <ibrahim.friedkin@gmail.com>
2018-02-15 12:25:30 +0100

    #27 Corregido bug que impedía generar javadoc.

--- src/main/java/com/furyviewer/web/rest/SeriesResource.java
+++ src/main/java/com/furyviewer/web/rest/SeriesResource.java
@@ -14,9 +14,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
