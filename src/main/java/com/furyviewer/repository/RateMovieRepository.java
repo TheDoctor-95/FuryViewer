d2e843a115a7409d9995e7ea14357361caca2983
Whoger <gonzalo.toscano.h@gmail.com>
2018-03-06 12:52:23 +0100

    #29 RateMovie implementado

--- src/main/java/com/furyviewer/repository/RateMovieRepository.java
+++ src/main/java/com/furyviewer/repository/RateMovieRepository.java
@@ -37,4 +37,6 @@ public interface RateMovieRepository extends JpaRepository<RateMovie, Long> {
     @Query("select r.rate from RateMovie r where r.user=:User and r.movie.id = :id ")
     Integer markPeli(@Param("User") User u, @Param("id") Long id);
 
+
+
 }
