60fc7a636ab3f3a7593d29434b1940a13224fa09
IFriedkin <ibrahim.friedkin@gmail.com>
2018-04-25 12:16:30 +0200

    #29 Filmografía añadida a los artistas.

---
+++ src/main/webapp/app/shared/model/filmographyArtist.model.ts
@@ -0,0 +1,12 @@
+export class FilmographyArtistModel {
+    constructor(
+        public id?: number,
+        public title?: string,
+        public releaseDate?: any,
+        public type?: string,
+        public urlCartel?: string,
+        public stat?: string
+    ){
+
+    }
+};
