9cd41f087b33326411a7b8f04e2ff84921f29ef1
Whoger <gonzalo.toscano.h@gmail.com>
2018-05-11 10:25:36 +0200

    #36 List Artist series con optimizacion, sin limit para el pop-up

--- src/main/java/com/furyviewer/service/util/ArtistLimitService.java
+++ src/main/java/com/furyviewer/service/util/ArtistLimitService.java
@@ -3,16 +3,19 @@ package com.furyviewer.service.util;
 
 import com.furyviewer.domain.Artist;
 import com.furyviewer.repository.ArtistRepository;
+import com.furyviewer.repository.EpisodeRepository;
 import com.furyviewer.repository.MovieRepository;
-import com.furyviewer.service.dto.util.ActorsLimitDTO;
+import com.furyviewer.service.dto.util.ActorLimitDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.lang.Long.compare;
 
 @Service
 public class ArtistLimitService {
@@ -23,12 +26,15 @@ public class ArtistLimitService {
     @Autowired
     private ArtistRepository artistRepository;
 
-    public List<ActorsLimitDTO> getInfoFromArtist(Long id){
+    @Autowired
+    private EpisodeRepository episodeRepository;
+
+    public List<ActorLimitDTO> getInfoFromArtist(Long id){
 
         Pageable top = new PageRequest(0, 4);
-        List<ActorsLimitDTO> list = movieRepository.getMainActorsByMovieLimit(id, top)
+        List<ActorLimitDTO> list = movieRepository.getMainActorsByMovieLimit(id, top)
         .stream()
-        .map(artist -> new ActorsLimitDTO(
+        .map(artist -> new ActorLimitDTO(
             artist.getId(),
             artist.getName(),
             artist.getImgUrl()
@@ -40,11 +46,11 @@ public class ArtistLimitService {
     }
 
 
-    public List<ActorsLimitDTO> getInfoFromArtistLimitless(Long id){
+    public List<ActorLimitDTO> getInfoFromArtistLimitless(Long id){
 
-        List<ActorsLimitDTO> list = movieRepository.getMainActorsByMovie(id)
+        List<ActorLimitDTO> list = movieRepository.getMainActorsByMovie(id)
             .stream()
-            .map(artist -> new ActorsLimitDTO(
+            .map(artist -> new ActorLimitDTO(
                 artist.getId(),
                 artist.getName(),
                 artist.getImgUrl()
@@ -54,4 +60,54 @@ public class ArtistLimitService {
         return list;
 
     }
+
+
+    @Transactional
+    public List<ActorLimitDTO> findActorBySerieId(Long serieID) {
+        return artistRepository.findAllWithEagerRelationships().stream()
+            .filter(artist -> artist.getEpisodes().stream()
+                .anyMatch(episode -> episode.getSeason().getSeries().getId().equals(serieID)))
+            .sorted((a1, a2) -> {
+                long numEpisodes1 = a1.getEpisodes().stream().filter(episode -> episode.getSeason().getSeries().getId().equals(serieID))
+                    .count();
+                long numEpisodes2 = a2.getEpisodes().stream().filter(episode -> episode.getSeason().getSeries().getId().equals(serieID))
+                    .count();
+                return compare(numEpisodes1, numEpisodes2);
+            })
+
+            .map(artist -> new ActorLimitDTO(
+                artist.getId(),
+                artist.getName(),
+                artist.getImgUrl()
+            ))
+            .collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public List<ActorLimitDTO> findActorBySeriesIdLimit(Long serieID) {
+        Pageable top = new PageRequest(0, 4);
+        return artistRepository.findAllWithEagerRelationshipsLimit(top)
+            .stream()
+            .filter(artist -> artist.getEpisodes().stream()
+                .anyMatch(episode -> episode.getSeason().getSeries().getId().equals(serieID)))
+            .sorted((a1, a2) -> {
+                long numEpisodes1 = a1.getEpisodes().stream().filter(episode -> episode.getSeason().getSeries().getId().equals(serieID))
+                    .count();
+                long numEpisodes2 = a2.getEpisodes().stream().filter(episode -> episode.getSeason().getSeries().getId().equals(serieID))
+                    .count();
+                return compare(numEpisodes1, numEpisodes2);
+            })
+            .map(artist -> new ActorLimitDTO(
+                artist.getId(),
+                artist.getName(),
+                artist.getImgUrl()
+            ))
+            .collect(Collectors.toList());
+
+        }
+
 }
+
+
+
